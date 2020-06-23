526e1860ea3b44432a44775045e51f69c776cf73
devallever <devallever@163.com>
2020-06-23 20:36:05 +0800

    FEATURE：async获取协程返回值

--- app/src/main/java/com/allever/app/kotlin/coroutine/CoroutineTest2.kt
+++ app/src/main/java/com/allever/app/kotlin/coroutine/CoroutineTest2.kt
@@ -6,23 +6,26 @@ private val mCoroutineJob = Job()
 private val mGlobalCoroutineScope = CoroutineScope(mCoroutineJob)
 
 fun main() {
-    globalCoroutineLaunchTest()
-    runBlockingTest()
-    launchCoroutineTest()
-    runBlocking {
-        createCoroutineWithCoroutineScopeTest()
-    }
-    createCoroutineWithCoroutineScopeTest2()
-    mGlobalCoroutineScope.launch {
-        delay(1000)
-        println("run on mGlobalCoroutineScope 1")
-    }
-    mGlobalCoroutineScope.launch {
-        delay(1500)
-        println("run on mGlobalCoroutineScope 2")
-    }
-    mCoroutineJob.cancel()
-    Thread.sleep(2000)
+//    globalCoroutineLaunchTest()
+//    runBlockingTest()
+//    launchCoroutineTest()
+//    runBlocking {
+//        createCoroutineWithCoroutineScopeTest()
+//    }
+//    createCoroutineWithCoroutineScopeTest2()
+//    mGlobalCoroutineScope.launch {
+//        delay(1000)
+//        println("run on mGlobalCoroutineScope 1")
+//    }
+//    mGlobalCoroutineScope.launch {
+//        delay(1500)
+//        println("run on mGlobalCoroutineScope 2")
+//    }
+//    mCoroutineJob.cancel()
+//    Thread.sleep(2000)
+
+    asyncForResultTest()
+    asyncForResultTest2()
     println("run on main")
 }
 
@@ -86,5 +89,49 @@ fun createCoroutineWithCoroutineScopeTest2() {
             println("run on coroutineScope 2")
         }
         println("run on outer coroutine")
+    }
+}
+
+fun asyncForResultTest() {
+    println("run on asyncForResultTest")
+    runBlocking {
+        val start = System.currentTimeMillis()
+        val result1 = async {
+            println("run on async 1")
+            delay(1000)
+            5 + 5
+        }.await()
+
+        val result2 = async {
+            println("run on async 2")
+            delay(1000)
+            4 + 6
+        }.await()
+        val result = result1 + result2
+        println("result = $result")
+        val end = System.currentTimeMillis()
+        println("用时 = ${end - start}")
+    }
+}
+
+fun asyncForResultTest2() {
+    println("run on asyncForResultTest2")
+    runBlocking {
+        val start = System.currentTimeMillis()
+        val result1 = async {
+            println("run on async 1")
+            delay(1000)
+            5 + 5
+        }
+
+        val result2 = async {
+            println("run on async 2")
+            delay(1000)
+            4 + 6
+        }
+        val result = result1.await() + result2.await()
+        println("result = $result")
+        val end = System.currentTimeMillis()
+        println("用时 = ${end - start}")
     }
 }
