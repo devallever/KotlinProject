a597db0d59f1ccf975d03b99b5094f0e5cc83c34
devallever <devallever@163.com>
2020-06-24 20:21:25 +0800

    FEATURE：协程的常用写法

--- app/src/main/java/com/allever/app/kotlin/coroutine/CoroutineTest2.kt
+++ app/src/main/java/com/allever/app/kotlin/coroutine/CoroutineTest2.kt
@@ -2,6 +2,9 @@ package com.allever.app.kotlin.coroutine
 
 import kotlinx.coroutines.*
 
+private val mCoroutineJob = Job()
+private val mGlobalCoroutineScope = CoroutineScope(mCoroutineJob)
+
 fun main() {
     globalCoroutineLaunchTest()
     runBlockingTest()
@@ -10,6 +13,16 @@ fun main() {
         createCoroutineWithCoroutineScopeTest()
     }
     createCoroutineWithCoroutineScopeTest2()
+    mGlobalCoroutineScope.launch {
+        delay(1000)
+        println("run on mGlobalCoroutineScope 1")
+    }
+    mGlobalCoroutineScope.launch {
+        delay(1500)
+        println("run on mGlobalCoroutineScope 2")
+    }
+    mCoroutineJob.cancel()
+    Thread.sleep(2000)
     println("run on main")
 }
 
